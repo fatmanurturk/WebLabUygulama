90cb736b87a7355724b45c960b674f36c1865db7
fatmanurturk <fatmanurturk@fatmanurturk-MacBook-Air.local>
2026-03-03 11:01:51 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ function App() {
     <div>
       <h1>Web Tasarimi ve Programlama</h1>
       <h2>LAB -1</h2>
-      <p>Ad Soyad: [Kendi Adin]</p>
-      <p>Ogrenci No: [Numaran]</p>
+      <p>Ad Soyad: Fatmanur Turk</p>
+      
+      <p>Ogrenci No: 230541065</p>
+       <p>Bölüm : Yazılım Mühendisliği</p>
+        <p>Kısa Tanıtım : Merhaba! Bu benim ilk Vite + React lab projem</p>
     </div>
   )
 }
